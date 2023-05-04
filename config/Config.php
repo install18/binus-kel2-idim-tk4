b1982b7777ec5b5b0ec6a9f0413505fb77899433
install18 <install.delapanbelas@gmail.com>
2023-05-04 23:31:37 +0700

    Add logic to index and add config

---
+++ config/Config.php
@@ -0,0 +1,7 @@
+<?php
+
+const BASEURL = 'http://localhost:8000/binus/kel_2/';
+const DB_HOST = 'localhost';
+const DB_USERNAME = 'root';
+const DB_PASS = 'root';
+const DB_NAME = 'binus_kel_2';
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-?>
+if (!session_id()) session_start();
+
+require_once 'core/App.php';
+require_once 'controller/BaseController.php';
+require_once 'database/Database.php';
+
+require_once 'config/Config.php';
+
+$app = new App;
 
