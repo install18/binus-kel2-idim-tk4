242af0bb27bbd0f53d5419931752031cf6ccfccb
install18 <install.delapanbelas@gmail.com>
2023-05-06 19:20:41 +0700

    add footer and header

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -4,7 +4,9 @@ class BaseController
 {
     public function view($view, $data = [])
     {
+        require_once '../app/view/template/header.php';
         require_once '../app/view/' . $view . '.php';
+        require_once '../app/view/template/footer.php';
     }
 
     public function model($model)
---
+++ view/template/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Toko berbicara</title>
+<!--    <link rel="stylesheet" href="--><?php //= BASEURL; ?><!--/css/bootstrap.css">-->
+<!--    <link rel="stylesheet" href="--><?php //= BASEURL; ?><!--/css/style.css">-->
+</head>
+<body>
+
+<!--Example-->
+<!--<nav class="navbar navbar-expand-lg navbar-light bg-light">-->
+<!--    <div class="container">-->
+<!--        <a class="navbar-brand" href="--><?php //= BASEURL; ?><!--">PHP MVC</a>-->
+<!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--            <span class="navbar-toggler-icon"></span>-->
+<!--        </button>-->
+<!--        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!--            <div class="navbar-nav">-->
+<!--                <a class="nav-item nav-link active" href="--><?php //= BASEURL; ?><!--">Home <span class="sr-only">(current)</span></a>-->
+<!--                <a class="nav-item nav-link" href="--><?php //= BASEURL; ?><!--/mahasiswa">Mahasiswa</a>-->
+<!--                <a class="nav-item nav-link" href="--><?php //= BASEURL; ?><!--/about">About</a>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</nav>-->
